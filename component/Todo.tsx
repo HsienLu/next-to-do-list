c3f6fe78f0c4d7aac2db62353a6daeb744d684c8
HsienLu <hsienlu@mail.ntust.edu.tw>
2024-01-29 18:29:33 +0800

    復原成getItem

--- component/Todo.tsx
+++ component/Todo.tsx
@@ -1,11 +1,157 @@
+"use client";
+import React, {use, useEffect, useState} from "react";
+const token: any = localStorage.getItem("token");
 export default function Todo() {
+  console.log(token);
+  const [todo, setTodo] = useState<{data: any[]}>({data: []}); // Update the type of todo state
+  const [addContent, setAddContent] = useState<{}>({content: ""});
+  useEffect(() => {
+    console.log(addContent);
+  }, [addContent]);
+  const handdleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setAddContent({...addContent, [e.target.name]: e.target.value});
+  //check
+  const check = async () => {
+    const data = await fetch("https://todolist-api.hexschool.io/todos/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: token,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .catch((error) => console.error("Error:", error));
+    console.log(data);
+    setTodo(data);
+  };
+  //add
+  const add = async (addContent: {}) => {
+    const response = await fetch("https://todolist-api.hexschool.io/todos/", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: token,
+      },
+      body: JSON.stringify(addContent),
+    });
+
+    if (!response.ok) {
+      console.log(`HTTP error! status: ${response.status}`);
+      const errorData = await response.json();
+      console.log(errorData);
+      return;
+    }
+
+    const data = await response.json();
+    console.log(data);
+    check();
+  };
+  //update
+  const update = async (itemId: string) => {
+    const response = await fetch(
+      `https://todolist-api.hexschool.io/todos/${itemId}/toggle`,
+      {
+        method: "PATCH",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: token,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.log(`HTTP error! status: ${response.status}`);
+      const errorData = await response.json();
+      console.log(errorData);
+      return;
+    }
+
+    const data = await response.json();
+    console.log(data);
+    check();
+  };
+  //delete
+  const deleteItem = async (itemId: string) => {
+    const response = await fetch(
+      `https://todolist-api.hexschool.io/todos/${itemId}`,
+      {
+        method: "DELETE",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: token,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.log(`HTTP error! status: ${response.status}`);
+      const errorData = await response.json();
+      console.log(errorData);
+      return;
+    }
+
+    const data = await response.json();
+    console.log(data);
+    check();
+  };
+  useEffect(() => {
+    // fetcher();
+    check();
+  }, []);
+
   return (
     <>
       <div className="todo">
         <label className="" htmlFor="enterTodo">
           輸入代辦事項
         </label>
-        <input className="input" type="text" id="enterTodo" />
+        <input
+          className="input"
+          type="text"
+          id="enterTodo"
+          name="content"
+          onChange={handdleChange}
+        />
+        <button type="button" className="btn" onClick={() => add(addContent)}>
+          加入
+        </button>
+        <ul>
+          {todo.data.map((item: any) => {
+            return (
+              <li key={item.id}>
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={() => {
+                    update(item.id);
+                  }}
+                >
+                  完成
+                </button>
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={() => {
+                    deleteItem(item.id);
+                  }}
+                >
+                  刪除
+                </button>
+                項目:{item.content}
+                {item.status ? "✔️" : "❌"}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
--- component/Users.tsx
+++ component/Users.tsx
@@ -1,49 +1,139 @@
-const body = {
-  email: "example3@gmail.com",
-  password: "example3",
-  nickname: "example3",
-};
-const fetcher = (url: string) =>
-  fetch(url, {
+"use client";
+import React, {useEffect, useState} from "react";
+
+interface fetchMessage {
+  status: boolean | string;
+  message: string;
+  uid?: string;
+  token?: string;
+}
+interface userData {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
+const regUrl = "https://todolist-api.hexschool.io/users/sign_up";
+const loginUrl = "https://todolist-api.hexschool.io/users/sign_in";
+export default function Users() {
+  const [fetchMessage, setFetchMessage] = useState<fetchMessage>({
+    status: "",
+    message: "",
+  });
+  const [userData, setUserData] = useState<userData>({
+    email: "",
+    password: "",
+    nickname: "",
+  });
+  const [loginMessage, setLoginMessage] = useState<fetchMessage>({
+    status: "",
+    message: "",
+  });
+  const parm = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
-    body: JSON.stringify(body),
-  })
-    .then((res) => {
-      let data = res.json();
+    body: JSON.stringify(userData),
+  };
+  const parmLogin = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({email: userData.email, password: userData.password}),
+  };
+  const fetcher = async (url: string, parm: {}) => {
+    const data = await fetch(url, parm)
+      .then((res) => res.json())
+      .catch((error) => console.error("Error:", error));
+    if (url === regUrl) {
+      setFetchMessage(data);
+    } else {
+      setLoginMessage(data);
+      localStorage.setItem("token", data.token || "");
       console.log(data);
-    })
-    .catch((error) => console.error("Error:", error));
+    }
+  };
 
-export default async function Users() {
-  const data = await fetcher("https://todolist-api.hexschool.io/users/sign_up");
-  console.log(data);
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
+  const handdleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUserData({...userData, [e.target.name]: e.target.value});
   return (
     <>
       <div className="com">
         <div className="div">
-          <label className="label" htmlFor="account">
-            Account
+          <label className="label" htmlFor="email">
+            Email
           </label>
-          <input type="text" className="input" id="account" />
+          <input
+            type="text"
+            className="input"
+            id="email"
+            name="email"
+            onChange={handdleChange}
+          />
         </div>
         <div className="div">
           <label className="label" htmlFor="password">
             Password
           </label>
-          <input type="password" className="input" id="password" />
+          <input
+            type="password"
+            className="input"
+            id="password"
+            name="password"
+            onChange={handdleChange}
+          />
         </div>
         <div className="div">
-          <button type="button" className="btn">
+          <label className="label" htmlFor="nickname">
+            Nickname
+          </label>
+          <input
+            type="text"
+            className="input"
+            id="nickname"
+            name="nickname"
+            onChange={handdleChange}
+          />
+        </div>
+        <div className="div">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => fetcher(regUrl, parm)}
+          >
             註冊
           </button>
-          <button type="button" className="btn">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => fetcher(loginUrl, parmLogin)}
+          >
             登入
           </button>
         </div>
+        <p>
+          註冊狀態:
+          {fetchMessage.status === ""
+            ? null
+            : fetchMessage.status === false
+              ? fetchMessage.message
+              : "註冊成功" + (fetchMessage.uid || "")}
+        </p>
+        <p style={{width: "100%", wordWrap: "break-word"}}>
+          登入狀態:
+          {loginMessage.status === ""
+            ? null
+            : loginMessage.status === false
+              ? loginMessage.message
+              : "登入成功" + (loginMessage.token || "")}
+        </p>
       </div>
     </>
   );
